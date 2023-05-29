db2d9dbd3cfeab698a0cea4e3c2bc1bb73847522
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-05-29 23:57:15 +0300

    upd

--- src/components/UI/MyButton.vue
+++ src/components/UI/MyButton.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-
-  </div>
+  <button class="btn">
+    <slot></slot>
+  </button>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style scoped>
+
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 
 </style>
