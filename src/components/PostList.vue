5a61fcfb34e5fc0c791619798e6925ba9391be15
VitaliyYurkevich <vitaliy.yurkevich@yandex.ru>
2023-06-02 21:43:56 +0300

    upd

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,20 +1,30 @@
 <template>
-  <div>
-    <div class="post" v-for="post in posts">
-      <div><strong>Название:</strong> {{ post.title }}</div>
-      <div><strong>Название:</strong> {{ post.body }}</div>
-    </div>
+  <div v-if="posts.length > 0">
+    <h3>Список пользователей</h3>
+    <post-item
+        class="post"
+        v-for="post in posts"
+        :post="post"
+        :key="post.id"
+        @remove="$emit('remove', post)"
+    />
   </div>
+  <h2 v-else="posts.length === 0">
+    Список пользователей пуст
+  </h2>
 </template>
 
 <script>
+import PostItem from "@/components/PostItem";
+
 export default {
-props: {
-  posts: {
-    type: Array,
-    required: true
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
   }
-}
 }
 </script>
 
@@ -24,6 +34,9 @@ props: {
   padding: 15px;
   border: 1px solid teal;
   margin: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
 </style>
